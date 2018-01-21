da40341f8e06bde4332ffbca6775877c4ddd390e
Jonathan Zhang <zhangjianan1996@qq.com>
2018-01-21 20:56:54 +0800

    update life

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,29 @@
-import React from 'react'
-import './static/less/main.less'
+import React from 'react';
+import './static/less/main.less';
 
 class App extends React.Component { // eslint-disable-line
+  componentWillMount() {
+    console.log('componentWillMount');
+  }
+  componentDidMount() {
+    console.log('componentDidMount');
+  }
+  componentWillReceiveProps() {
+    console.log('componentWillReceiveProps');
+  }
+  shouldComponentUpdate() {
+    console.log('shouldComponentUpdate');
+    return true;
+  }
+  componentWillUpdate() {
+    console.log('componentWillUpdate');
+  }
+  componentDidUpdate() {
+    console.log('componentDidUpdate');
+  }
+  componentWillUnmount() {
+    console.log('componentWillUnmount');
+  }
   render() {
     return (<div className="react-main">
       <h1>This is React-Builder</h1>
@@ -9,7 +31,8 @@ class App extends React.Component { // eslint-disable-line
       <p className="react-h">Support eslint</p>
       <p className="react-h">HMR loading</p>
       <p className="react-h">
-        If you like this you can star and fork <a href="https://github.com/TYRMars/React-Builder">React-Builder</a>
+        If you like this you can star and fork
+        <a href="https://github.com/TYRMars/React-Builder">React-Builder</a>
       </p>
     </div>) // eslint-disable-line
   }
