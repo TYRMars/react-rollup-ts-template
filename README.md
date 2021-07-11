37803df764dd5654863f2fd19916c403086e6057
Jianan Zhang <zhangjianan1996@qq.com>
2021-07-11 22:55:01 +0800

    feat: init

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # react-ts-template
+
 React TypeScipr基本开发环境，简单的React基本环境构建。
+
+[@commitlint/config-conventional](https://github.com/conventional-changelog/commitlint/tree/master/@commitlint/config-conventional#type-enum)
--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,14 @@
+import resolve from '@rollup/plugin-node-resolve';
+import babel from '@rollup/plugin-babel';
+
 export default {
-  input: "index.ts",
+  input: "src/index.tsx",
   output: {
-    file: "dist/",
-  }
+    file: '/dist/bundle.js',
+    format: 'cjs'
+  },
+  plugin: [
+    resolve(),
+    babel({ babelHelpers: 'bundled' })
+  ]
 }
